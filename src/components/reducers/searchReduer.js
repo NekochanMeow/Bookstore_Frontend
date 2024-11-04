43ab8ff8e97c3ea8650e43a5ab37b7864472a6a8
Sakawrat <you@example.com>
2024-11-04 21:22:20 +0700

    first commit

--- src/components/reducers/searchReduer.js
+++
@@ -1,9 +0,0 @@
-// เปรียบเหมือน store
-export function searchReduer(state = { text: "" }, action) {
-    switch (action.type) {
-        case 'SEARCH_QUERY':
-            return { ...state, ...action.payload }; //ส่งอะไรมาก็เก็บอันนั้น
-        default:
-            return state;
-    }
-}
